3d83c1a7380864dace4c9aa8e9f3c76a2d3dc436
LyubovKhizhnyak <lubakruk@ya.ru>
2023-10-30 21:07:33 +0400

    Buratino

---
+++ Lesso2/src/main/java/Buratino/Buratino.java
@@ -0,0 +1,16 @@
+package Buratino;
+
+public class Buratino {
+    public static void main(String[] args) {
+
+        int buratinas_apples = 6;
+        int selected_carabas = 4;
+        int remains_apples = buratinas_apples - selected_carabas;
+        System.out.println("У Буратино было "
+                + buratinas_apples + " яблок");
+        System.out.println("Карабас отобрал "
+                + selected_carabas + " яблока");
+        System.out.println("Осталось у Буратино "
+                + remains_apples + " яблока");
+            }
+}
